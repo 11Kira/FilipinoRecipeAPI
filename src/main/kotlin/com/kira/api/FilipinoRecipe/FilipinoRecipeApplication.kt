17d7b0be6bdc02be662dee868c2272eb21a6557a
kirav <kiravillagracia@yahoo.com>
2025-05-23 23:09:31 +0800

    initial commit

---
+++ src/main/kotlin/com/kira/api/FilipinoRecipe/FilipinoRecipeApplication.kt
@@ -0,0 +1,11 @@
+package com.kira.api.FilipinoRecipe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FilipinoRecipeApplication
+
+fun main(args: Array<String>) {
+	runApplication<FilipinoRecipeApplication>(*args)
+}
---
+++ src/test/kotlin/com/kira/api/FilipinoRecipe/FilipinoRecipeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kira.api.FilipinoRecipe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FilipinoRecipeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
