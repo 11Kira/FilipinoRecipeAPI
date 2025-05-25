664795ddcce8c031fc30d52bb91227e2be9fc5ee
kirav <kiravillagracia@yahoo.com>
2025-05-25 23:44:48 +0800

    added recipe repository

---
+++ src/main/kotlin/com/kira/api/FilipinoRecipeAPI/database/repository/RecipeRepository.kt
@@ -0,0 +1,8 @@
+package com.kira.api.FilipinoRecipeAPI.database.repository
+
+import com.kira.api.FilipinoRecipeAPI.database.model.Recipe
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface RecipeRepository : MongoRepository<Recipe, String> {
+    fun findByRecipeId(id: String): Recipe?
+}
