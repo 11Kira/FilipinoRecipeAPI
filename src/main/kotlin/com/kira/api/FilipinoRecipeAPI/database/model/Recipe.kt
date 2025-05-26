ef58c8863ce170d5e19506854543255465f9c771
k.i.r.a. <kiravillagracia@yahoo.com>
2025-05-26 08:12:12 +0800

    used string as datatype for id

--- src/main/kotlin/com/kira/api/FilipinoRecipeAPI/database/model/Recipe.kt
+++ src/main/kotlin/com/kira/api/FilipinoRecipeAPI/database/model/Recipe.kt
@@ -1,22 +1,18 @@
 package com.kira.api.FilipinoRecipeAPI.database.model
 
-import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
-import org.springframework.data.mongodb.core.mapping.Field
 import java.time.Instant
 
 @Document("recipes")
 data class Recipe(
     @Id
-    val id: ObjectId = ObjectId(),
+    val id: String? = null,
     val image: String = "",
     val name: String = "",
     val description: String = "",
     val ingredients: List<String> = emptyList(),
     val instructions: List<String> = emptyList(),
     val tips: List<String> = emptyList(),
-    @Field("recipe_id")
-    val recipeId: Long = 0L,
     val createdAt: Instant
 )
