c3b56b046f920b34adc291d4d8b4ce2df0ac826b
kirav <kiravillagracia@yahoo.com>
2026-02-07 22:29:27 +0800

    added protein enum

--- src/main/kotlin/com/kira/api/FilipinoRecipeAPI/models/enums/Category.kt
+++ src/main/kotlin/com/kira/api/FilipinoRecipeAPI/models/enums/Category.kt
@@ -1,11 +1,6 @@
 package com.kira.api.FilipinoRecipeAPI.models.enums
 
 enum class Category {
-    BEEF,
-    PORK,
-    CHICKEN,
-    SEAFOOD,
-    VEGETABLES,
     QUICK_MEALS,
     FILIPINO_CLASSICS
 }
