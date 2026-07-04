48a7d5535d1fa92e83d7681f5eeb99d225bafe16
kirav <kiravillagracia@yahoo.com>
2026-07-04 10:21:21 +0800

    added response and request data classes for forgot and reset password feature

--- src/main/kotlin/com/kira/api/FilipinoRecipeAPI/controller/AuthController.kt
+++ src/main/kotlin/com/kira/api/FilipinoRecipeAPI/controller/AuthController.kt
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/v1/auth")
 class AuthController(
-    private val authService: AuthService,
+    private val authService: AuthService
 ) {
 
     @PostMapping("/register")
---
+++ src/main/kotlin/com/kira/api/FilipinoRecipeAPI/models/requests/ResetPasswordRequest.kt
@@ -0,0 +1,7 @@
+package com.kira.api.FilipinoRecipeAPI.models.requests
+
+data class ResetPasswordRequest(
+    val email: String,
+    val resetToken: String,
+    val newPassword: String
+)
